35f7459e26be4bece48fd979f2ae3c4296eef9c5
kat88512 <kasiapost01@gmail.com>
2024-02-27 22:31:35 +0100

    Add tutorial classes

---
+++ MyRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace FakeTwitter
+{
+    public class MyRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
