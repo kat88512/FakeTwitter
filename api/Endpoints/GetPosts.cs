aeed0d9f67f7688d1180a94d05a5ed72fb01dc54
kat88512 <kasiapost01@gmail.com>
2024-04-18 01:01:03 +0200

    Remove GetMockPosts method

--- api/Endpoints/GetPosts.cs
+++ api/Endpoints/GetPosts.cs
@@ -31,32 +31,5 @@ namespace api.Endpoints
 
             await SendAsync(postsDTO, cancellation: ct);
         }
-
-        private List<PostDTO> GetMockPosts()
-        {
-            var mockPosts = new List<PostDTO>()
-            {
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    DateCreated = new DateTime(2024, 3, 3),
-                    Text = "Text 1"
-                },
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    DateCreated = new DateTime(2024, 3, 4),
-                    Text = "Text 2"
-                },
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    DateCreated = new DateTime(2024, 3, 5),
-                    Text = "Text 3"
-                },
-            };
-
-            return mockPosts;
-        }
     }
 }
