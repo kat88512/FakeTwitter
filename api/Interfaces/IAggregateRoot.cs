45ecff8f470cc3358977e2671f4b628f0d394200
kat88512 <kasiapost01@gmail.com>
2024-04-17 22:50:10 +0200

    Add generic repository interface

---
+++ api/Interfaces/IAggregateRoot.cs
@@ -0,0 +1,4 @@
+﻿namespace api.Interfaces
+{
+    public interface IAggregateRoot { }
+}
---
+++ api/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace api.Interfaces
+{
+    public interface IRepository<TEntity, TId>
+        where TEntity : IAggregateRoot
+        where TId : struct
+    {
+        Task AddAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity?> GetByIdAsync(TId id);
+        Task UpdateAsync(TEntity entity);
+    }
+}
