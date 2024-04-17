1568d199ad6c9bfc6ddecf9abb2977cb219aeb3c
kat88512 <kasiapost01@gmail.com>
2024-04-18 01:00:16 +0200

    Add repository and automapper to Post endpoints

--- api/Endpoints/AddPost.cs
+++ api/Endpoints/AddPost.cs
@@ -1,4 +1,5 @@
-﻿using api.Models;
+﻿using api.Interfaces;
+using api.Models;
 using api.RequestModels;
 using api.ResponseModels;
 using FastEndpoints;
@@ -7,6 +8,13 @@ namespace api.Endpoints
 {
     public class AddPost : Endpoint<AddPostRequest, PostDTO>
     {
+        private readonly IRepository<Post, Guid> _postRepository;
+
+        public AddPost(IRepository<Post, Guid> postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
         public override void Configure()
         {
             Post("/api/posts");
@@ -17,12 +25,16 @@ namespace api.Endpoints
         {
             var post = new Post(req.Id, req.Text);
 
-            await SendAsync(new()
-            {
-                Id = post.Id,
-                DateCreated = post.DateCreated,
-                Text = post.Text,
-            });
+            await _postRepository.AddAsync(post);
+
+            await SendAsync(
+                new()
+                {
+                    Id = post.Id,
+                    DateCreated = post.DateCreated,
+                    Text = post.Text,
+                }
+            );
         }
     }
 }
--- api/Endpoints/GetPosts.cs
+++ api/Endpoints/GetPosts.cs
@@ -1,10 +1,22 @@
-﻿using api.ResponseModels;
+﻿using api.Interfaces;
+using api.Models;
+using api.ResponseModels;
 using FastEndpoints;
+using IMapper = AutoMapper.IMapper;
 
 namespace api.Endpoints
 {
-    public class GetPosts : EndpointWithoutRequest<List<PostDTO>>
+    public class GetPosts : EndpointWithoutRequest<IEnumerable<PostDTO>>
     {
+        private readonly IRepository<Post, Guid> _postRepository;
+        private readonly IMapper _mapper;
+
+        public GetPosts(IRepository<Post, Guid> postRepository, IMapper mapper)
+        {
+            _postRepository = postRepository;
+            _mapper = mapper;
+        }
+
         public override void Configure()
         {
             Get("/api/posts");
@@ -13,16 +25,35 @@ namespace api.Endpoints
 
         public override async Task HandleAsync(CancellationToken ct)
         {
-            await SendAsync(GetMockPosts(), cancellation: ct);
+            var posts = await _postRepository.GetAllAsync();
+
+            var postsDTO = _mapper.Map<IEnumerable<PostDTO>>(posts);
+
+            await SendAsync(postsDTO, cancellation: ct);
         }
 
         private List<PostDTO> GetMockPosts()
         {
             var mockPosts = new List<PostDTO>()
             {
-                new() { Id = Guid.NewGuid(), DateCreated = new DateTime(2024, 3, 3), Text = "Text 1" },
-                new() { Id = Guid.NewGuid(), DateCreated = new DateTime(2024, 3, 4), Text = "Text 2" },
-                new() { Id = Guid.NewGuid(), DateCreated = new DateTime(2024, 3, 5), Text = "Text 3" },
+                new()
+                {
+                    Id = Guid.NewGuid(),
+                    DateCreated = new DateTime(2024, 3, 3),
+                    Text = "Text 1"
+                },
+                new()
+                {
+                    Id = Guid.NewGuid(),
+                    DateCreated = new DateTime(2024, 3, 4),
+                    Text = "Text 2"
+                },
+                new()
+                {
+                    Id = Guid.NewGuid(),
+                    DateCreated = new DateTime(2024, 3, 5),
+                    Text = "Text 3"
+                },
             };
 
             return mockPosts;
