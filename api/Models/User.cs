6179c8dd74242f9cd2e6ef7949571ab12f44a206
kat88512 <kasiapost01@gmail.com>
2024-05-17 21:39:53 +0200

    Add User model and DTO

---
+++ api/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using api.Interfaces;
+
+namespace api.Models
+{
+    public class User : IAggregateRoot<Guid>
+    {
+        public Guid Id { get; private init; }
+        public string EmailAddress { get; private set; }
+        public string Password { get; private set; }
+
+        public User(Guid id, string emailAddress, string password)
+        {
+            Id = id;
+            EmailAddress = emailAddress;
+            Password = password;
+        }
+    }
+}
