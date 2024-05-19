0c70608e48d380486deb527599b8f9f2d770ef42
kat88512 <kasiapost01@gmail.com>
2024-05-19 10:17:10 +0200

    Rename Password property for model

--- api/Models/User.cs
+++ api/Models/User.cs
@@ -6,13 +6,13 @@ namespace api.Models
     {
         public Guid Id { get; private init; }
         public string EmailAddress { get; private set; }
-        public string Password { get; private set; }
+        public string PasswordHash { get; private set; }
 
-        public User(Guid id, string emailAddress, string password)
+        public User(Guid id, string emailAddress, string passwordHash)
         {
             Id = id;
             EmailAddress = emailAddress;
-            Password = password;
+            PasswordHash = passwordHash;
         }
     }
 }
