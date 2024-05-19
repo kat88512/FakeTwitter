34b8514a192dc53052e1905ba6d9957d1a32327f
kat88512 <kasiapost01@gmail.com>
2024-05-19 10:35:07 +0200

    Add validation to AddPost request

--- api/RequestModels/AddPostRequest.cs
+++ api/RequestModels/AddPostRequest.cs
@@ -1,5 +1,19 @@
-﻿namespace api.RequestModels
+﻿using api.Configuration;
+using FastEndpoints;
+using FluentValidation;
+
+namespace api.RequestModels
 {
+    public class AddPostValidator : Validator<AddPostRequest>
+    {
+        public AddPostValidator()
+        {
+            RuleFor(p => p.Text)
+                .NotEmpty()
+                .MaximumLength(StringLengths.PostMaxLength);
+        }
+    }
+
     public class AddPostRequest
     {
         public Guid Id { get; set; }
