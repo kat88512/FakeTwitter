f2dc64293e89d0ee241a24aeecd0f6aa63ed8194
kat88512 <kasiapost01@gmail.com>
2024-05-21 01:19:23 +0200

    Add common string lengths

--- api/Configuration/StringLengths.cs
+++ api/Configuration/StringLengths.cs
@@ -2,6 +2,8 @@
 {
     public static class StringLengths
     {
-
+        public const int ShortString = 250;
+        public const int MediumString = 500;
+        public const int LongString = 1000;
     }
 }
